d9cac6d7a4943593a1091f23ee9a10bca3e0c675
alex-hh <alex.hhooker@gmail.com>
2023-04-13 20:17:23 +0100

    templates for parsing answer and explanation

--- cryptic/prompts/cot.py
+++ cryptic/prompts/cot.py
@@ -1,49 +1,56 @@
-ben_v1 = """
-I will answer some clues:
-1. Boy captures the work of Shakespeare, for example (7)
-Answer: SONNETS (examples of works of Shakespeare) - SON (boy) + NETS (captures)
-2. Country folk appearing in operatic piece (7)
-Answer: ARMENIA (country) - ARIA (operatic piece) + MEN (folk)
-3. Where men are on board ship beside revolutionary (5)
-Answer: CHESS (where men are on board) - CHE (Revolutionary, Che Guevara) + SS (designation for a ship)
-4. Amputate limbs to remove weapons (6)
-Answer: DISARM (Remove weapons  - DIS-ARM (amputating limbs)
-5. Accuse one politician apiece (7)
-Answer: IMPEACH (accuse) - I (one) + MP (politician) + EACH (apiece)
-6. Orchestra was taken aback by cutting machine (7)
-Answer: BANDSAW (cutting machine) - BAND (orchestra) + SAW (was, 'taken aback' i.e. backwards)
-7. Concerning drama in second game after draw (6)
-Answer: REPLAY (a second game, typically following a drawn result in the first) - RE (meaning concerning) + PLAY (a type of drama)
-Answer this clue in the same style, making sure not to give any verbal explanation, following 'Answer:'.
+ben_v1 = {
+    "prompt": """I will answer some clues:
+    1. Boy captures the work of Shakespeare, for example (7)
+    Answer: SONNETS (examples of works of Shakespeare) - SON (boy) + NETS (captures)
+    2. Country folk appearing in operatic piece (7)
+    Answer: ARMENIA (country) - ARIA (operatic piece) + MEN (folk)
+    3. Where men are on board ship beside revolutionary (5)
+    Answer: CHESS (where men are on board) - CHE (Revolutionary, Che Guevara) + SS (designation for a ship)
+    4. Amputate limbs to remove weapons (6)
+    Answer: DISARM (Remove weapons  - DIS-ARM (amputating limbs)
+    5. Accuse one politician apiece (7)
+    Answer: IMPEACH (accuse) - I (one) + MP (politician) + EACH (apiece)
+    6. Orchestra was taken aback by cutting machine (7)
+    Answer: BANDSAW (cutting machine) - BAND (orchestra) + SAW (was, 'taken aback' i.e. backwards)
+    7. Concerning drama in second game after draw (6)
+    Answer: REPLAY (a second game, typically following a drawn result in the first) - RE (meaning concerning) + PLAY (a type of drama)
+    Answer this clue in the same style, making sure not to give any verbal explanation, following 'Answer:'.
 
-Clue: {clue} ({num_letters})
-"""
+    Clue: {clue} {num_letters}
+    """,
+    "answer_pattern": r"Answer: (?P<answer>\w+) (?P<explanation>\(.+\))",
+    "example_output": "Answer: REPLAY (a second game, typically following a drawn result in the first) - RE (meaning concerning) + PLAY (a type of drama)"
+}
 
 
-ben_v1_mod = """
-I will answer some clues. Answer in the same style, making sure not to give any verbal explanation.
+ben_v1_mod = {
+    "prompt": """
+    I will answer some clues. Answer in the same style, making sure not to give any verbal explanation.
 
-Clue: Boy captures the work of Shakespeare, for example (7)
-Answer: SONNETS (examples of works of Shakespeare) - SON (boy) + NETS (captures)
+    Clue: Boy captures the work of Shakespeare, for example (7)
+    Answer: SONNETS (examples of works of Shakespeare) - SON (boy) + NETS (captures)
 
-Clue: Country folk appearing in operatic piece (7)
-Answer: ARMENIA (country) - ARIA (operatic piece) + MEN (folk)
+    Clue: Country folk appearing in operatic piece (7)
+    Answer: ARMENIA (country) - ARIA (operatic piece) + MEN (folk)
 
-Clue: Where men are on board ship beside revolutionary (5)
-Answer: CHESS (where men are on board) - CHE (Revolutionary, Che Guevara) + SS (designation for a ship)
+    Clue: Where men are on board ship beside revolutionary (5)
+    Answer: CHESS (where men are on board) - CHE (Revolutionary, Che Guevara) + SS (designation for a ship)
 
-Clue: Amputate limbs to remove weapons (6)
-Answer: DISARM (Remove weapons  - DIS-ARM (amputating limbs)
+    Clue: Amputate limbs to remove weapons (6)
+    Answer: DISARM (Remove weapons  - DIS-ARM (amputating limbs)
 
-Clue: Accuse one politician apiece (7)
-Answer: IMPEACH (accuse) - I (one) + MP (politician) + EACH (apiece)
+    Clue: Accuse one politician apiece (7)
+    Answer: IMPEACH (accuse) - I (one) + MP (politician) + EACH (apiece)
 
-Clue: Orchestra was taken aback by cutting machine (7)
-Answer: BANDSAW (cutting machine) - BAND (orchestra) + SAW (was, 'taken aback' i.e. backwards)
+    Clue: Orchestra was taken aback by cutting machine (7)
+    Answer: BANDSAW (cutting machine) - BAND (orchestra) + SAW (was, 'taken aback' i.e. backwards)
 
-Clue: Concerning drama in second game after draw (6)
-Answer: REPLAY (a second game, typically following a drawn result in the first) - RE (meaning concerning) + PLAY (a type of drama)
+    Clue: Concerning drama in second game after draw (6)
+    Answer: REPLAY (a second game, typically following a drawn result in the first) - RE (meaning concerning) + PLAY (a type of drama)
 
-Clue: {clue} ({num_letters})
-Answer: 
-"""
+    Clue: {clue} {num_letters}
+    Answer: 
+    """,
+    "answer_pattern": r"Answer: (?P<answer>\w+) (?P<explanation>\(.+\))",
+    "example_output": "REPLAY (a second game, typically following a drawn result in the first) - RE (meaning concerning) + PLAY (a type of drama)",
+}
